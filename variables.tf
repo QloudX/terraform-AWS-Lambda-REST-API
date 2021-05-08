2f9148effea56cb77908cde493d2eed5befc29bc
Harish KM <harishkm@harishkm.in>
2021-05-08 13:24:41 +0530

    Accept API endpoints & Lambda functions as input

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
 variable "api_name" {}
+variable "api_stage_name" {}
+
+variable "api_endpoints" {
+  type        = any
+  description = "API to Lambda mappings. (See examples.)"
+}
+
+variable "lambda_functions" {
+  type        = any
+  description = "Lambda functions. (See examples.)"
+}
