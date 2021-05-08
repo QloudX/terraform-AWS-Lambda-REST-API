d68027de36f505e6f937438ef1d4dd0d8879bb91
Harish KM <harishkm@harishkm.in>
2021-05-08 13:26:50 +0530

    Add API deployment & stage

--- api.tf
+++ api.tf
@@ -1,6 +1,9 @@
-resource "aws_api_gateway_rest_api" "rest_api" {
-  name = var.api_name
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  stage_name    = var.api_stage_name
 }
